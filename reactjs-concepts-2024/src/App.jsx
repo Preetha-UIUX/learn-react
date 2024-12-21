41ad5ebc1265fcfb595cd3116513a303892b8170
Preetha Chandramohan <preetha.chandramohan@perficient.com>
2024-12-21 11:21:21 +0530

    Form handleing

--- reactjs-concepts-2024/src/App.jsx
+++ reactjs-concepts-2024/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css'
 // import Users from './components/users';
 // import ChangeButtonComponent from './components/context-concept/button';
 // import ChangeTextComponent from './components/context-concept/text';
-import UseReducerExample from './components/use-reducer-example';
+// import UseReducerExample from './components/use-reducer-example';
+import FormComponent from './components/form';
 
 // const dummyProductData = ['Product 1', 'Product 2', 'Product 3'];
 
@@ -14,7 +15,8 @@ function App() {
       <Users /> */}
       {/* <ChangeButtonComponent />
       <ChangeTextComponent /> */}
-      <UseReducerExample />
+      {/* <UseReducerExample /> */}
+      <FormComponent />
       
     </div>
   )
